81e5851a339639e8fcc836af039eac11357c6f7d
Drake Rentrop <drakerentrop@gmail.com>
2018-10-10 13:48:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.length == 1
+  "#{array[0]}"
+elsif array.length == 2
+  array.join (" and ")
+elsif array.length >= 3
+  array[-1] = "and #{array[-1]}"
+  array.join(", ")
+end
 end
